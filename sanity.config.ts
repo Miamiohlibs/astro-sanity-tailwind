e448c1477d3655ae727bfa1b2c20a0fac557dea2
Jerry Yarnetsky <yarnete@miamioh.edu>
2024-06-03 14:15:32 -0400

    adding schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,11 @@
 // sanity.config.ts
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
+import author from './schemas/author';
+import gallery from './schemas/gallery';
+import mainImage from './schemas/mainImage';
+import post from './schemas/post';
+import relatedLinks from './schemas/relatedLinks';
 
 export default defineConfig({
   name: 'project-name',
@@ -9,8 +14,13 @@ export default defineConfig({
   dataset: 'production',
   plugins: [structureTool()],
   schema: {
+    // If you want more content types, you can add them to this array
     types: [
-      /* your content types here*/
-    ],
+        author, 
+        gallery, 
+        mainImage, 
+        post, 
+        relatedLinks,
+      ],
   },
 });
