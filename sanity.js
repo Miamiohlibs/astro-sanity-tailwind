78f2ecdfa3574895021eede6455f367ce7837a81
Jerry Yarnetsky <yarnete@miamioh.edu>
2024-06-10 16:20:13 -0400

    sanity.js edits

--- sanity.js
+++ sanity.js
@@ -6,7 +6,7 @@ import { sanityClient } from '@sanity/client';
 export const client = sanityClient({
   projectId: 'h8zbt27a',
   dataset: 'production',
-  useCdn: true, // set to `false` to bypass the edge cache
+  useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 });
