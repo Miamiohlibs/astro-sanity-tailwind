241b75a0e6630b59d2930c4f7c21303c44bd5f65
Jerry Yarnetsky <36451755+yarnetsky@users.noreply.github.com>
2024-06-12 18:39:42 +0000

    adding link field

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import {visionTool} from '@sanity/vision'
+import {linkField} from 'sanity-plugin-link-field'
 
 import { schemaTypes } from './schema';
 
@@ -11,7 +12,11 @@ export default defineConfig({
   title: 'MU Libraries Astro Test',
   projectId: 'h8zbt27a',
   dataset: 'production',
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(), 
+    visionTool(), 
+    linkField(),
+  ],
   schema: {
     types: schemaTypes,
   },
