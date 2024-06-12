0f6b5475352acd5ea8dd6b4bb69c6fc0d6d46d78
Jerry Yarnetsky <36451755+yarnetsky@users.noreply.github.com>
2024-06-12 16:23:21 +0000

    new schema for page builder

--- schema/index.ts
+++ schema/index.ts
@@ -1,5 +1,25 @@
+// ./schemas/index.ts
+
+import {promotionType} from './promotionType'
+import {formType} from './formType'
+import {heroType} from './heroType'
+import {imageGalleryType} from './imageGalleryType'
+import {pageType} from './pageType'
+import {textWithIllustrationType} from './textWithIllustrationType'
+import {videoType} from './videoType'
 import blockContent from './blockContent';
 import post from './post';
 import carousel from './carousel';
 
-export const schemaTypes = [post, carousel, blockContent];
+export const schemaTypes = [
+  pageType,
+  heroType,
+  promotionType,
+  textWithIllustrationType,
+  imageGalleryType,
+  formType,
+  videoType,
+  post,
+  carousel,
+  blockContent,
+]
